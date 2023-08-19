2ea3d697a3eb52e88d726e34a6316da28a9eacb5
Melkias Abraham <melkiasae@hotmail.com>
2023-08-18 20:20:15 -0400

    Attempted to place a value on each letter attempted in a word in the guessboard component.

--- src/components/GuessBoard.js
+++ src/components/GuessBoard.js
@@ -5,15 +5,46 @@ function GuessBoard() {
   const [board, setBoard] = useState(boardDefault);
   return (
     <div className='Guessboard'>
-      <div className='row'></div>
-      <div className='row'></div>
-      <div className='row'></div>
-      <div className='row'></div>
-      <div className='row'></div>
+      <div className='row'>
+        <Letter letterPos={0} attemptValue={0} />
+        <Letter letterPos={1} attemptValue={0} />
+        <Letter letterPos={2} attemptValue={0} />
+        <Letter letterPos={3} attemptValue={0} />
+        <Letter letterPos={4} attemptValue={0} />
+      </div>
+      <div className='row'>
+        <Letter letterPos={0} attemptValue={1} />
+        <Letter letterPos={1} attemptValue={1} />
+        <Letter letterPos={2} attemptValue={1} />
+        <Letter letterPos={3} attemptValue={1} />
+        <Letter letterPos={4} attemptValue={1} />
+      </div>
+      <div className='row'>
+        <Letter letterPos={0} attemptValue={2} />
+        <Letter letterPos={1} attemptValue={2} />
+        <Letter letterPos={2} attemptValue={2} />
+        <Letter letterPos={3} attemptValue={2} />
+        <Letter letterPos={4} attemptValue={2} />
+      </div>
+      <div className='row'>
+        <Letter letterPos={0} attemptValue={3} />
+        <Letter letterPos={1} attemptValue={3} />
+        <Letter letterPos={2} attemptValue={3} />
+        <Letter letterPos={3} attemptValue={3} />
+        <Letter letterPos={4} attemptValue={3} />
+      </div>
+      <div className='row'>
+        <Letter letterPos={0} attemptValue={4} />
+        <Letter letterPos={1} attemptValue={4} />
+        <Letter letterPos={2} attemptValue={4} />
+        <Letter letterPos={3} attemptValue={4} />
+        <Letter letterPos={4} attemptValue={4} />
+      </div>
       <div className='row'></div>
 
     </div>
   )
 }
+import Letter from './Letter';
 
 export default GuessBoard
