58fae689d29332a92a719ecf0c152872bc46313e
Melkias Abraham <melkiasae@hotmail.com>
2023-08-24 23:43:03 -0400

    The game is now centered on the screen through css.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import GuessBoard from './components/GuessBoard';
-import KeyBoard from './components/KeyBoard';
-import { createContext, useState } from 'react';
-import { boardDefault } from './Words'
+import "./App.css";
+import GuessBoard from "./components/GuessBoard";
+import KeyBoard from "./components/KeyBoard";
+import { createContext, useState } from "react";
+import { boardDefault } from "./Words";
 
 export const AppContext = createContext();
-
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
@@ -14,9 +13,11 @@ function App() {
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value = { {board, setBoard}}>
-      <GuessBoard />
-      <KeyBoard />
+      <AppContext.Provider value={{ board, setBoard }}>
+        <div className="game">
+          <GuessBoard />
+          <KeyBoard />
+        </div>
       </AppContext.Provider>
     </div>
   );
