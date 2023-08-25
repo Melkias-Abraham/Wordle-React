cb7394bbc9244d71638939c8b6aad760bcdff5c3
Melkias Abraham <melkiasae@hotmail.com>
2023-08-24 23:56:45 -0400

    Through AppContext, thekeyvalue is able to be passed through to the component. Letters are now able to be clicked on the keyboard on the site and will only appear in the first box. Not able to translate from an actual keyboard.

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext} from "react";
+import { AppContext } from "../App";
 
+function Key({ keyVal, bigKey }) {
+  const { board, setBoard } = useContext(AppContext)
 
-function Key( {keyVal, bigKey} ) {
+  const seletLetter = () => {
+    const newBoard = [...board]
+    newBoard[0][0] = keyVal
+    setBoard(newBoard)
+  };
+
   return (
-    <div className='key' id={bigKey && 'big'}> {keyVal}</div>
-  )
+    <div className="key" id={bigKey && "big"} onClick={seletLetter}>
+      {keyVal}
+    </div>
+  );
 }
 
-export default Key
+export default Key;
